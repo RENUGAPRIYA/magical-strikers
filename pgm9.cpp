e2ec5ac75285d5442d13e75067fc2361e988b633
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-02-21 17:52:46 +0530

    Create pgm9.cpp

---
+++ pgm9.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+using namespace std;
+int main()
+{
+int sum=0, i,N,K,a[N];
+cout<<"enter the value of k,N";
+cin>>K>>N;
+for(i=0;i<=K;i++)
+{
+sum=sum+i;
+cout<<"sum is="<<sum<<endl;
+cin>>sum;
+}
