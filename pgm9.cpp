20a578302466bea8885ef9c6e21c7a2e53bc9554
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-02-21 17:54:37 +0530

    Update pgm9.cpp

--- pgm9.cpp
+++ pgm9.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
 int sum=0, i,N,K,a[N];
-cout<<"enter the value of k,N";
+cout<<"enter the value k,N";
 cin>>K>>N;
 for(i=0;i<=K;i++)
 {
 sum=sum+i;
-cout<<"sum is="<<sum<<endl;
+cout<<"sum is"<<sum<<endl;
 cin>>sum;
 }
