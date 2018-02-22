150cd14f92de6276a14165b655048f2c386f9571
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-02-22 17:34:19 +0530

    Create pgm14.cpp

---
+++ pgm14.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ int a;
+ int g;
+ cout<<" enter number "<<endl;
+ cin>>a;
+ cout<<" enter number "<<endl;
+ cin>>g;
+ cout<<"*********************************"<<endl;
+ cout<<"guvi"<<endl;
+ cout<<"**********************************"<<endl;
+ for(;a<=g;)
+ {
+ cout<<a<<endl;
+
+ a=a+2;
+ }
+}
