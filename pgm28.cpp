89bf3c40d773dea574c4f113bea1fdc5fba837b6
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-02-27 17:29:53 +0530

    Create pgm28.cpp

---
+++ pgm28.cpp
@@ -0,0 +1,44 @@
+#include<iostream.h>
+#include<conio.h>
+
+ void main()
+  {
+   int arr[100],i,n,sum=0;
+   clrscr();
+   cout<<"How many elements you want to enter: ";
+   cin>>n;
+   cout<<"Enter any "<<n<<" elements in Array: ";
+   for(i=0;i<n;i++)
+   {
+   cin>>arr[i];
+   }
+   cout<<"Sum of all Elements are: ";
+
+   for(i=0;i<n;i++)
+   {
+   cout<<arr[i]
+   }
+   for(i=0;i<n;i++)
+   {
+   }
+  cout<<indexes;
+int[] Array = { 5, 8, 3, 7 };
+
+int Element = 3;
+
+int Index;
+
+
+for (int i = 0; i < Array.Length; i++)
+
+{
+  
+   if (Element == Array[i])
+ 
+    {
+         Index = i;
+     }
+
+}
+  getch();
+  }
