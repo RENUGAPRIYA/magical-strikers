a7e369ad055e2a25e16d85ef0fb6541b9b731f5a
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-03-03 17:51:12 +0530

    Create play4.cpp

---
+++ play4.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string str;
+    for (int i = 0; i < 10; ++i) {
+        char c;
+        std::
+ c='.';
+        str += c;
+    }
+
+    std::cout << str << std::endl;
+    return 0;
+}
