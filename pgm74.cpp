ff5df1c728583776453f344f3805e57e6ac840ab
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 14:28:37 +0530

    Create pgm74.cpp

---
+++ pgm74.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main ()
+{
+    int sum;
+    int average;
+    double dec1;
+   
+ 
+    cout << "Enter five decimal numbers: ";
+    cin >> dec1;
+    cout << endl;
+ 
+    cout << "The numbers you entered are: \n"
+    cout << dec1;
+    cout << endl;
+     
+    double a = dec1
+   
+    int h = nint(a);
+   
+ 
+    sum = h;
+    average =h;
+ 
+    cout << endl;
+    cout << "Sum: " << sum << endl;
+    cout << "Average: " << average << endl;
+ 
+    return 0;
+}
