c6d8d1600a9989df9fc71f83827bc3567c09f0c6
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 12:27:23 +0530

    Create pgm64.c

---
+++ pgm64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	if(c%2==0)
+	{
+		printf("this is even");
+	}
+	else
+	{
+		printf("this is odd");
+	}
+	return 0;
+	}
