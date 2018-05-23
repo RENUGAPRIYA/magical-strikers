dfcee17ce6ba1900941f5ffef03ffcc8f52183b8
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 16:22:14 +0530

    Create pgm91.cpp

---
+++ pgm91.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a,b,c;
+int volume+0;
+cout<<"enter values of cuboid";
+cin>>a>>b>>c;
+ volume=a*b*c;
+cout<<volume;
+    return 0;
+}
