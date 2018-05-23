e6acd700c93d805ef7da0f80c93efd1ba7b505a1
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 11:09:13 +0530

    Create pgm54.cpp

---
+++ pgm54.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+
+    int  num,nnum, sum = 0;
+ 
+    cout << "Enter the number : ";
+    cin >> val;
+    num = val;
+    if(num%2= 0)
+    {
+        cout<<num;
+
+    }
+   else
+{
+nnum=num-1;
+cout<<nnum;
+}
