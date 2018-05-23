e80435dee48e3afd04b45216f877e8a2bb1e09ae
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 15:36:07 +0530

    Update pgm81.cpp

--- pgm81.cpp
+++ pgm81.cpp
@@ -3,23 +3,15 @@ using namespace std;
 
 int main() {
   
-      int a,b,c,d,,e,f;
+      int a,b;
       int x=0;
-      int y=0;
-      int z=0;
-
+    
 cout<<"enter first line";
 cin>>a,b;
-cout<<"enter second line";
-cin>>c,d;
-cout<<"enter third line";
-cin>>e,f;
+
     x=b-a;
-    y=d-c;
-    z=f-e;
+   
 cout<<x;
-cout<<y; 
-cout<<z; 
-  
+
   return 0;
 }
