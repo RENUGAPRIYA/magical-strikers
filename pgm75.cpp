9e02df47026eb5a31cd3e587f17e333277c60951
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 14:48:52 +0530

    Create pgm75.cpp

---
+++ pgm75.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main ()
+{
+   int a[i];
+cout<<"enter array elements";
+cin>>a[i];
+int size = my_sizeof(arr)/my_sizeof(arr[0]);
+if(size%2==0)
+{
+i=size/2;
+i++;
+a[i]={'*'};
+
+}else
+{
+n=size/2;
+int i = std::nint(n); 
+a[i]={'*'};
+}
+
+    return 0;
+}
