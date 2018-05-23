21b9e4ddd3807a5cf662771add8040e8e018dd83
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 15:34:41 +0530

    Create pgm81.cpp

---
+++ pgm81.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+      int a,b,c,d,,e,f;
+      int x=0;
+      int y=0;
+      int z=0;
+
+cout<<"enter first line";
+cin>>a,b;
+cout<<"enter second line";
+cin>>c,d;
+cout<<"enter third line";
+cin>>e,f;
+    x=b-a;
+    y=d-c;
+    z=f-e;
+cout<<x;
+cout<<y; 
+cout<<z; 
+  
+  return 0;
+}
