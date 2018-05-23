1f429a61a63fa5267416e7cd8ad1e7021c877372
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 15:55:57 +0530

    Create pgm83.cpp

---
+++ pgm83.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a,b,c,d;
+int op1,op2;
+cout<<"enter input for division";
+cin>>a>>b;
+cout<<"enter input for modulo";
+cin>>c>>d;
+  op1=a/b;
+op2=a%b;
+  return 0;
+}
