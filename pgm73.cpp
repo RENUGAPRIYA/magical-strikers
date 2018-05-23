b86f9869d50ddb20e131e91ccdf88a6cbd1c6584
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 14:23:24 +0530

    Create pgm73.cpp

---
+++ pgm73.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+cin>>low>>high;
+cout<<"enter number";
+cin>>num;
+if(low <= x  && x <= high)
+{
+cout<<"yes";
+}
+else
+{
+cout<<"no";
+}return 0;
+}
+
