b1c9f9e7614d08738b63a5b56af44f1b46f81b73
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 09:43:34 +0530

    Create pgm46.cpp

---
+++ pgm46.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int num, 
+        cout << "Enter a number \n";
+    cin  >> num;
+        
+   
+      if(num!=0)
+{ 
+       
+      num++;
+    }
+    
+    cout <<" next of digits in a number is " << num;
+    
+    return 0;
+}
