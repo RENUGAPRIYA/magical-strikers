9eb02e164ca18bfc9e2e40065afc1d7e733f5941
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 12:19:54 +0530

    Update pgm61.cpp

--- pgm61.cpp
+++ pgm61.cpp
@@ -1,33 +1,32 @@
-#include <iostream>
+// C++ program to print distinct characters of a
+
+# include <iostream>
 using namespace std;
+# define NO_OF_CHARS 256
+ 
+
+void printDistinct(char *str)
+{
+   
+    int count[NO_OF_CHARS];
+ 
+   
+    int i;
+    for (i = 0; *(str+i); i++)
+        if(*(str+i)!=' ')
+            count[*(str+i)]++;
+    int n = i;
+ 
+    
+    for (i = 0; i < n; i++)
+        if (count[*(str+i)] == 1)
+            cout<< str[i];
+}
+ 
 
 int main()
 {
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
-
-    cout << "Enter the number of terms: ";
-    cin >> n;
-
-    cout << "Fibonacci Series: ";
-
-    for (int i = 1; i <= n; ++i)
-    {
-        
-        if(i == 1)
-        {
-            cout << " " << t1;
-            continue;
-        }
-        if(i == 2)
-        {
-            cout << t2 << " ";
-            continue;
-        }
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-        
-        cout << nextTerm << " ";
-    }
+    char str[] = "GeeksforGeeks";
+    printDistinct((str);
     return 0;
 }
