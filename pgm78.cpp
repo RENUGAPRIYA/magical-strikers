63bedd320cb39a11b8ab2f1fd5791418ed6a7ee4
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 14:59:54 +0530

    Create pgm78.cpp

---
+++ pgm78.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    if(n%13==0)
+{
+cout<<"yes";
+}
+else
+{
+cout<<"no";
+}
+
+    return 0;
+}
