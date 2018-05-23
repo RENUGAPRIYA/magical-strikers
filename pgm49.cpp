e8509dd4bb6cdd094e38124e137dfd966c7e8752
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-23 09:54:27 +0530

    Create pgm49.cpp

---
+++ pgm49.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a,b;
+int sum=0; 
+
+    cout << "Enter the numbers of data: ";
+    cin >>a>>b;
+    sum=a+b;
+   
+    cout << "sum = " << sum;
+
+    return 0;
+}
