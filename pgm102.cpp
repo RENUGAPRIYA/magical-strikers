a41775f62379b1c48251493769458d852b30ec30
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-24 09:50:07 +0530

    Create pgm102.cpp

---
+++ pgm102.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace std;
+
+int main()
+{
+  int a;
+  cout<<"enter number";
+  cin>>a;
+if(a%2==0)
+{
+ int n=a/2;
+ cout<<n;
+}
+else
+{cout<<a;
+}
+return 0;
+}
