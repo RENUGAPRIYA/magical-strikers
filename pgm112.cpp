e9157e60725e86f09fb8b6d33cb43f25f42d9f4d
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-24 10:43:00 +0530

    Create pgm112.cpp

---
+++ pgm112.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+  int arr[i],size,k;
+    cout<<"enter size and k value";
+     cin>>>size>>k;
+  for(int i=0;i<=size;i++)
+{  cin>>arr[i];
+     if(k==arr[i])
+    {
+     cout<<"yes";
+     }
+else
+    { cout<<"no";
+     }
+
+}   
+
+getch();
+}
