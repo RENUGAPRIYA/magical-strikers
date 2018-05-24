3fedb45580c6e6ea14ea97efeaeac37f710e822f
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-24 09:27:31 +0530

    Create pgm98.cpp

---
+++ pgm98.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a[i],n;
+cout<<"enter number";
+cin>>n;
+for(int i=0;i<=n;i++)
+{
+cin>>a[i];
+}
+for(int i=0;i<=n;i++)
+{
+ if(a[i]>a[i+1])
+{
+ cout<<i;}
+}else
+{
+cout<<"no unsorted values";
+}
+}
+    return 0;
+}
