7ec266e4bef0c307a8a63da841716955206ba17d
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-24 09:31:09 +0530

    Create pgm99.cpp

---
+++ pgm99.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,b,c;
+int sum=0;
+cout<<" enter number";
+cin>>a>>b>>c;
+sum=a*b;
+int result=sum%c;
+cout<<result;
+    return 0;
+}
