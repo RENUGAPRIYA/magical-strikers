a24790a0b5fcb7343b39c8428cd97cc50067a8dc
RENUGAPRIYA <36407021+RENUGAPRIYA@users.noreply.github.com>
2018-05-24 10:21:05 +0530

    Create pgm111.cpp

---
+++ pgm111.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+  int no,a=0;
+clrscr();
+
+cout<<"Enter any num : ";
+cin>>no;
+
+while(no>0)
+{
+no=no/10;
+a++;
+}
+
+cout<<"\n no. of digits in given number is: "<<a;
+getch();
+}
